eabd60c07d7f48825a6fe3e0743a84f0f0119176
Dinesh R <dineshruthwik21@gmail.com>
2024-01-24 08:59:48 -0800

    Create Sum of first 100 natural numbers.cpp

---
+++ Sum of first 100 natural numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int i, sum = 0;
+    
+    for (i = 1; i <= 100; i++) {
+        cout << i << " " << endl ;
+        sum = sum + i;
+    }
+    
+     cout << "Sum of natural number:";
+     cout << sum;
+    
+
+    return 0;
+}
